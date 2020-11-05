c39625b8925c24d519333e4c2075ac00745d467d
Clement Sam <clementsam75@gmail.com>
2020-11-05 18:38:45 +0000

    style: create basic project structure

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,11 +3,11 @@
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
   </div>
-  <router-view />
+  <router-view/>
 </template>
 
 <style lang="scss">
-#app {
+#errntry {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
